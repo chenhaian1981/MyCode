1e784b834c2defde6d9252748ceaad374c6a01d4
HaiAn Chen <150769002+chenhaian1981@users.noreply.github.com>
2025-01-04 03:39:25 +0000

    js练习3，4

--- FCC-JavaScript.js
+++ FCC-JavaScript.js
@@ -59,3 +59,48 @@ function getGrade(score) {
 console.log(getGrade(96));
 console.log(getGrade(82));
 console.log(getGrade(56));
+
+// 练习三 if else
+/*
+老师对你目前所做的方案感到非常开心。 但是现在他们希望有一种简单的方法来检查学生的成绩是否合格。 只要不是 "F" 评级，就是成绩合格。
+完善 hasPassingGrade 函数，这个函数把学生的分数作为参数。 如果这个学生的成绩合格，那么你的函数应该返回 true，否则返回 false。
+
+提示：
+使用 getGrade 函数来获得学生的评级。 然后检查这个评级是否及格。
+*/
+
+function hasPassingGrade(score) {
+    if (score >= 60) {
+      return true;
+    }else {
+      return false;
+    }
+  }
+  console.log(hasPassingGrade(100));
+  console.log(hasPassingGrade(53));
+  console.log(hasPassingGrade(87));
+
+  // 练习四 输出
+  /*
+  现在，教师们已经掌握了他们所需要的所有信息，他们希望能够将结果告诉学生。
+  完善 studentMsg 函数，这个函数以 totalScores 和 studentScore 作为参数。 这个函数应该返回一个字符串，代表给学生的信息。
+  如果学生通过了该课程，字符串应遵循这种格式：
+  
+  提示：
+  使用 getAverage 函数来获得班级平均分。
+  调用 getGrade 函数来获得学生的评级。
+  用字符串连接符（+）来创建发给学生的信息。
+  请留意信息中的标点符号和空格。 
+  */
+
+  function studentMsg(totalScores, studentScore) {
+    function says(){
+      if (getGrade(studentScore) == "F"){
+      return "You failed the course."
+    }else {
+      return "You passed the course."
+    }
+    }
+  return "Class average: "+getAverage(totalScores)+". "+"Your grade: "+getGrade(studentScore)+". "+says();
+  }
+  console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
