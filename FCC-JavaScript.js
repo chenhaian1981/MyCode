a33f1f4ba2b682ca9ef7b1a6f94ebebede9fd1e7
HaiAn Chen <150769002+chenhaian1981@users.noreply.github.com>
2025-01-04 02:56:50 +0000

    js练习2

--- FCC-JavaScript.js
+++ FCC-JavaScript.js
@@ -20,3 +20,42 @@ function getAverage(scores) {
   }
   console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
   console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+
+
+
+
+// 练习二    分数评级
+ /*
+ 现在，老师需要你帮助将学生们的分数转换成一个字母评级。完善 getGrade 函数， 
+ 这个函数以一个数字类型的 score 作为参数。 你的函数应该根据分数返回一个字符串，代表字母评级。
+ 这里是分数段和它们所对应的字母评级：
+ 分数段	字母评级
+ 100	"A++"
+ 90 - 99	"A"
+ 80 - 89	"B"
+ 70 - 79	"C"
+ 60 - 69	"D"
+ 0 - 59	"F"
+ 提示：
+    记得你学习过一些条件语句（if、else if 和 else）。
+    记得你学习过一些比较运算符（>、<、>=、<=、===）。 
+*/
+
+function getGrade(score) {
+    if(score == 100){
+      return "A++";
+    }else if (90 <= score){
+      return "A";
+    }else if (80 <= score){
+      return "B";
+    }else if (70 <= score){
+      return "C";
+    }else if (60 <= score){
+      return "D";
+    }else {
+      return "F"
+    }
+  }
+console.log(getGrade(96));
+console.log(getGrade(82));
+console.log(getGrade(56));
